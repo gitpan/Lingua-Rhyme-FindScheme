9cf1ec1f1db2b654aa822fa09131c3f5d37a2393
Lee ♫ Goddard <lgoddard@cpan.org>
2001-06-01 22:30:56 +0000

    Import of LGODDARD/Lingua-Rhyme-FindScheme-0.02 from CPAN.
    gitpan-cpan-distribution: Lingua-Rhyme-FindScheme
    gitpan-cpan-version:      0.02
    gitpan-cpan-path:         LGODDARD/Lingua-Rhyme-FindScheme-0.02.tar.gz
    gitpan-cpan-author:       LGODDARD
    gitpan-cpan-maturity:     released

--- FindScheme.pm
+++ FindScheme.pm
@@ -1,12 +1,13 @@
 package Lingua::Rhyme::FindScheme;
 use Lingua::Rhyme;
 
-our $VERSION = 0.01;
-our $chat = 0;
+our $VERSION = 0.02;
+use warnings;
+use strict;
 
 =head1 NAME
 
-Lingua::Rhyme::FindScheme - find rhyme schemes in poetry
+Lingua::Rhyme::FindScheme - find rhyme schemes in text.
 
 =head1 SYNOPSIS
 
@@ -30,12 +31,22 @@ Lingua::Rhyme::FindScheme - find rhyme schemes in poetry
 	);
 
 	my %scheme = %{Lingua::Rhyme::FindScheme::list_couplets (@shakespeare_sonnet_ii)};
-
 	foreach (sort {$a <=> $b} keys %scheme){
 		warn "Scheme $_ = $scheme{$_}\n";
 	}
 
+	my $i=0;
+	foreach my $scheme ( Lingua::Rhyme::FindScheme::endings(@shakespeare_sonnet_ii) ){
+		warn "Line ",$i+1," ... $scheme ... $shakespeare_sonnet_ii[$i]\n";
+		++$i;
+	}
+
 	__END__
+
+
+=head1 PREREQUISITES
+
+	Lingua::Rhyme
 
 
 =head1 DESCRIPTION
@@ -43,11 +54,6 @@ Lingua::Rhyme::FindScheme - find rhyme schemes in poetry
 This is the first version of a module which will grow with my needs.
 
 All requests/suggestions much appreciated.
-
-
-=head1 PREREQUISITES
-
-	Lingua::Rhyme
 
 
 =head1 FUNCTIONS
@@ -60,25 +66,20 @@ NB: the line number index begins at zero.
 =cut
 
 sub list_couplets { my @text = @_;
-	my $check_text = '';
-	my $done_text  = '';
-	my $i;
+	my $i = 0;
 	my %scheme;
 
 	foreach my $line (@text){
 		++$i;
 		$line =~ m/([\w']+)[^\w]+$/;
 		my $word = $1;
-		my $j =0;
+		my $j = 0;
 		foreach my $check_line (@text){
 			++$j;
 			next if $i == $j;
 			$check_line =~ m/([\w']+)[^\w]+$/;
 			my $check_word = $1;
 			if ( Lingua::Rhyme::simplematch($word,$check_word) ){
-				#warn "Lines $i and $j rhyme\n";
-				#warn "Lookup Word >$word< matches check_word  >$check_word< ";
-				#warn "\t$line\n\t$check_line\n\n";
 				$scheme{$i} = $j unless exists $scheme{$j} and $scheme{$j} == $i;
 			}
 		}
@@ -88,6 +89,49 @@ sub list_couplets { my @text = @_;
 }
 
 
+
+=head1 &endings (@text)
+
+Accepts an array of text, and scans it for a scheme of rhymes at line ends, returning an array of the scheme.
+
+=cut
+
+sub endings { my @text = @_;
+	my $i = -1;
+	my @scheme;						# return array
+	my @schememap = ('A'..'Z');		# what to name rhymes
+	my $schememapi = 0; 			# index to current pos in schememap
+
+	LINE:
+	foreach my $line (@text){
+		++$i;
+		next LINE if defined $scheme[$i];
+		$line =~ m/([\w']+)[^\w]*$/;
+		my $word = $1;
+		my $j = -1;
+		# Find all rhymes for $word in the text
+		XCHECK:
+		foreach my $check_line (@text){
+			++$j;
+			next XCHECK if $i == $j;
+			next XCHECK if defined $scheme[$j];
+			$check_line =~ m/([\w']+)[^\w]*$/;
+			my $check_word = $1;
+			if ( Lingua::Rhyme::matchall($word,$check_word) ){
+				$scheme[$i] = $schememap[$schememapi];
+				$scheme[$j] = $scheme[$i];
+			}
+		}
+		# Didn't find a rhyme
+		$scheme[$i] = $schememap[$schememapi] if not defined $scheme[$i];
+		$schememapi++;
+	}
+
+	return @scheme;
+}
+
+
+
 1;
 __END__
 
@@ -95,6 +139,10 @@ __END__
 
 L<Lingua::Rhyme>;
 perl(1).
+
+=head1 AUTHOR
+
+Lee Goddard <lgoddard@cpan.org>
 
 =head1 COPYRIGHT
 
